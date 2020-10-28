397b4897072bc7654748345589cdc60645c34582
Justin Merz <jrmerz@gmail.com>
2020-10-28 10:38:06 -0700

    adding waitUtil for mongo connection

--- node-utils/lib/state/mongo.js
+++ node-utils/lib/state/mongo.js
@@ -1,6 +1,7 @@
 const {MongoClient, ObjectId} = require('mongodb');
 const logger = require('../logger');
 const config = require('../config');
+const waitUtil = require('../wait-util');
 
 class Database {
 
@@ -24,7 +25,9 @@ class Database {
 
     this.connected = false;
 
-    this.connecting = new Promise((resolve, reject) => {
+    this.connecting = new Promise(async (resolve, reject) => {
+      await waitUtil(config.mongo.host, config.mongo.port);
+
       MongoClient.connect(url, (err, client) => {  
         if( err ) {
           logger.error('Failed to connect to Mongo', url, err);
