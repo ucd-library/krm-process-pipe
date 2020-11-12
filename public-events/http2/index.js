aba59a8e1a9a2ec77e5dedda862222b7eb660880
Justin Merz <jrmerz@gmail.com>
2020-11-12 14:42:02 -0800

    adding shortcuts for h2, adding docs for h2 events

--- public-events/http2/index.js
+++ public-events/http2/index.js
@@ -1,4 +1,5 @@
 const http2 = require('http2');
+const fs = require('fs');
 const uuid = require('uuid');
 const {logger, config, bus, utils} = require('@ucd-lib/krm-node-utils');
 
@@ -14,9 +15,33 @@ const registrations = {};
 const kafka = bus.kafka;
 const KAFKA_GROUP_ID = 'http2-service';
 
+/**
+ * Load the shortcuts.  This allows for simpler pre-defined routes
+ */
+let shortCuts = null;
+if( fs.existsSync('/etc/krm/event-shortcuts.js') ) {
+  shortCuts = require('/etc/krm/event-shortcuts.js');
+  if( typeof shortCuts !== 'object' ) {
+    console.error('Shortcuts must be an object, found: '+(typeof shortCuts ))
+    process.exit(-1);
+  }
+  for( let route in shortCuts ) {
+    if( typeof shortCuts[route].test !== 'function' ) {
+      console.error('Shortcuts route objects must have test() method: '+route);
+      process.exit(-1);
+    }
+  }
+
+  console.log('Using shortcut routes: '+Object.keys(shortCuts));
+}
+
+/**
+ * Called every time a new h2 connection starts
+ */
 server.on('stream', (stream, headers, flags) => {
   const method = headers[HTTP2_HEADER_METHOD];
 
+  // only accept GET paths
   if( method !== 'GET' ) {
     stream.respond({
       [HTTP2_HEADER_STATUS]: 404
@@ -25,15 +50,22 @@ server.on('stream', (stream, headers, flags) => {
     return;
   }
 
-  let subject;
+  let subject, shortcut;
   let path = headers[HTTP2_HEADER_PATH];
   let orgPath = path;
   const id = uuid.v4();
 
+  // decode the path, either checking for a shortcut path are parsing the provided URI 
   try {
     path = decodeURIComponent(path.replace('/_/h2/', ''));
-    subject = utils.subjectParser(path);
+    if( shortCuts && shortCuts[path] ) {
+      shortcut = shortCuts[path];
+    } else {
+      subject = utils.subjectParser(path);
+    }
+
   } catch(e) {
+    // if something goes wrong, bail out
     stream.respond({
       [HTTP2_HEADER_STATUS]: 500,
       [HTTP2_HEADER_CONTENT_TYPE]: 'application/json'
@@ -42,35 +74,44 @@ server.on('stream', (stream, headers, flags) => {
       error: true,
       path : orgPath,
       subject : path,
+      shortcut,
       message : e.message
     });
     stream.end();
     return;
   }
 
+  // send response headers, h2 style
   stream.respond({
     [HTTP2_HEADER_STATUS]: 200,
     [HTTP2_HEADER_CONTENT_TYPE]: 'application/x-ndjson'
   });
 
-
+  // register h2 connection (stream) with subject or shortcut requested
   registrations[id] = {
-    path, stream, subject
+    path, stream, subject, shortcut
   };
-  write(stream, {connected: true, subject: path});
 
+  // send connected message so they know all is set
+  write(stream, {connected: true, subject: path, shortcut : shortcut ? true : false});
+
+  // when the stream does finally close (on the user side), delete the registration
   stream.on('close', () => delete registrations[id]);
 });
 
+// start main h2-only service
 server.listen(3000, () => {
   logger.info('listening on *:3000');
 });
 
+// helper function for writing json-newline to stream
 function write(stream, msg) {
   stream.write(JSON.stringify(msg)+'\n');
 }
 
+// TODO: we probably should start at 'earliest' but just start streaming from connection time
 (async function() {
+  // setup kafka connection and subscribe the the subject-ready topic
   this.kafkaConsumer = new kafka.Consumer({
     'group.id': KAFKA_GROUP_ID,
     'metadata.broker.list': config.kafka.host+':'+config.kafka.port,
@@ -79,16 +120,20 @@ function write(stream, msg) {
   await this.kafkaConsumer.connect();
   await this.kafkaConsumer.subscribe([config.kafka.topics.subjectReady]);
 
+  // start consuming messages
   await this.kafkaConsumer.consume(msg => {
     try {
       let raw = msg.value.toString('utf-8');
       msg = JSON.parse(raw);
 
+      // check registrations (h2 connections) against message subject
       let id, reg, subject;
       for( id in registrations ) {
         reg = registrations[id];
         
-        if( reg.subject.path.regex.test(msg.subject) ) {
+        if( reg.shortcut && shortcut.test(msg.subject) ) {
+          write(reg.stream, {subject: msg.subject});
+        } else if( reg.subject && reg.subject.path.regex.test(msg.subject) ) {
 
           // if( subject.opts.entireMessage ) {
           //   reg.socket.emit('message', msg);
