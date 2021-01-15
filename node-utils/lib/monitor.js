c3852082bb477fa0583596cca36b846f1921cfdc
Justin Merz <jrmerz@gmail.com>
2021-01-15 09:32:19 -0800

    fix for service account check

--- node-utils/lib/monitor.js
+++ node-utils/lib/monitor.js
@@ -21,7 +21,7 @@ class Monitoring {
     
     let clientConfig = {};
     if( config.google.serviceAccountFile && 
-      fs.existsSync(serviceAccountFile) &&
+      fs.existsSync(config.google.serviceAccountFile) &&
       fs.lstatSync(config.google.serviceAccountFile).isFile() ) {
       clientConfig.keyFilename = config.google.serviceAccountFile;
     }
